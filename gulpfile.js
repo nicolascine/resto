18dbe1d7dd56307aa4f329f1a0a81da5d8bf300d
nicolas silva <sovietfrance@gmail.com>
2015-01-28 17:40:15 -0300

    add gulp config

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+    jshintReporter = require('jshint-stylish'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    less = require('gulp-less'),
+    watch = require('gulp-watch'),
+    gutil = require('gulp-util');
+
+// clean + autoprefix
+var LessPluginCleanCSS = require("less-plugin-clean-css"),
+    cleancss = new LessPluginCleanCSS({advanced: true});
+
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefix = new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
+
+
+gulp.task('default', function() {
+	//
+});
+
+// LESS
+gulp.task('build-less',function () {
+  gulp.src('./devAssets/less/site.less')
+	.pipe(less({ plugins: [autoprefix, cleancss] }))
+    .pipe(gulp.dest('./app/assets/css/'))
+    .on('error', gutil.log);
+});
+
+// JS
+gulp.task('concatena-js',function () {
+  gulp.src(['./bower_components/jquery/dist/jquery.min.js'
+  			,'./bower_components/components-bootstrap/js/bootstrap.min.js'
+  			,'./devAssets/js/resto.js'])
+  .pipe(concat('todo.min.js'))
+  .pipe(uglify())
+  .pipe(jshint())
+  .pipe(gulp.dest('./app/assets/js/'))
+  .on('error', gutil.log);
+
+});
+
