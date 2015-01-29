73d095e4bc5a42845d1b4e014e6bf3731dcadeb3
nicolas silva <sovietfrance@gmail.com>
2015-01-29 04:20:03 -0300

    more irrelevants css changes..

--- devAssets/js/resto.js
+++ devAssets/js/resto.js
@@ -26,4 +26,25 @@
 })();
 
 
+/*! 
+ *  ----
+ */
+
+$(document).ready(function($) {
+  
+ WebFontConfig = {
+    google: { families: [ 'Karla:400,700:latin' ] }
+  };
+
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+});
 
