0bc11566559a044b4b31bee43d39cefa752ebe72
nicolas silva <sovietfrance@gmail.com>
2015-02-02 15:29:56 -0300

    add new dependencies, uncss, ignore some bootstrap mobile styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    jshintReporter = require('jshint-stylish'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    less = require('gulp-less'),
-    watch = require('gulp-watch'),
-    gutil = require('gulp-util'),
-    uncss = require('gulp-uncss-task');
+var gulp              = require('gulp'),
+    plumber           = require('gulp-plumber'),
+    jshint            = require('gulp-jshint'),
+    jshintReporter    = require('jshint-stylish'),
+    concat            = require('gulp-concat'),
+    uglify            = require('gulp-uglify'),
+    less              = require('gulp-less'),
+    watch             = require('gulp-watch'),
+    gutil             = require('gulp-util'),
+    expect            = require('gulp-expect-file'),
+    uncss             = require('gulp-uncss'),
+    csso              = require('gulp-csso');
 
 // clean + autoprefix
 var LessPluginCleanCSS = require("less-plugin-clean-css"),
@@ -17,33 +20,62 @@ var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
 
 
 gulp.task('default', function() {
-	gulp.watch('./devAssets/js/resto.js', ['build-js']);
-	gulp.watch('./devAssets/less/*/*.less', ['build-less']);
+
+  var watchJs    = ['devAssets/js/resto.js'],
+      watchLess  = ['devAssets/less/*/*.less'];
+
+	gulp.watch(watchJs, ['build-js']);
+	gulp.watch(watchLess, ['build-less']);
+
 });
 
 // LESS
 gulp.task('build-less', function () {
-  gulp.src('./devAssets/less/site.less')
-	.pipe(less({ plugins: [autoprefix, cleancss] }))
-  .pipe(gulp.dest('./app/assets/css/'))
-  .on('error', gutil.log);
+
+  var lessFile    =  'devAssets/less/site.less',
+      htmlFiles   =  'app/index.html',
+      destFolder  =  'app/assets/css/';
+
+  return gulp.src(lessFile)
+         .pipe(plumber())
+         .pipe(expect(lessFile))
+         .pipe(less({ plugins: [autoprefix, cleancss] }))
+         .pipe(uncss({
+            html: [htmlFiles],
+            ignore: [
+                ".fade",
+                ".fade.in",
+                ".collapse",
+                ".collapse.in",
+                ".collapsing",
+                ".alert-danger",
+                /\.open/
+            ]
+          }))
+         .pipe(csso())
+         .pipe(gulp.dest(destFolder))
+         .on('error', gutil.log);
 });
+
 
 // JS
 gulp.task('build-js', function () {
-  gulp.src(['./bower_components/jquery/dist/jquery.min.js'
-  			,'./bower_components/components-bootstrap/js/bootstrap.min.js'
-  			,'./devAssets/js/resto.js'])
-  .pipe(concat('todo.min.js'))
-  .pipe(uglify())
-  .pipe(jshint())
-  .pipe(gulp.dest('./app/assets/js/'))
-  .on('error', gutil.log);
+  
+  var jQuery    = 'bower_components/jquery/dist/jquery.min.js',
+      bootStrap = 'bower_components/components-bootstrap/js/bootstrap.min.js',
+      restoJS   = 'devAssets/js/resto.js';
 
+  return gulp.src([jQuery, bootStrap, restoJS])
+         .pipe(plumber())
+         .pipe(concat('todo.min.js'))
+         .pipe(uglify())
+         .pipe(jshint())
+         .pipe(gulp.dest('./app/assets/js/'))
+         .on('error', gutil.log);
 });
 
 //REMOVE unused CSS
-
+/*
 gulp.task('unused', function() {
   gulp.src('./app/site.css')
   .pipe(uncss({
@@ -52,5 +84,4 @@ gulp.task('unused', function() {
   .pipe(gulp.dest('./app'))
   
   .on('error', gutil.log);
-});
-
+});*/
